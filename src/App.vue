0bc50974849e7c324cabb7fcb18ba0be436672f7
Andrew Gulin <andrewgulin@MacBook-Air-Andrew.local>
2020-03-13 13:20:25 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ТУДУШЕЧКА</h1>
+    <AddTodo 
+      @add-todo="addTodo"
+    />
+    <TodoList 
+      v-bind:todos="todos"
+      @remove-todo="removeTodo"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
 export default {
+  data() {
+    return {
+      todos: [
+        {id: 1, title: "Помыть кошку", completed: false},
+        {id: 2, title: "Сходить в шарагу", completed: false},
+        {id: 3, title: "Купить гараж", completed: false},
+      ]
+    }
+  },
+  methods: {
+    removeTodo(id){
+      this.todos = this.todos.filter(t => t.id !==id)
+    },
+    addTodo(todo){
+      this.todos.push(todo)
+    }
+  }, 
   name: 'App',
   components: {
-    HelloWorld
+    TodoList,
+    AddTodo
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <ul>
+            <TodoItem 
+                v-for="(todo, i) of todos" :key="todo.id"
+                v-bind:todo="todo"
+                v-bind:index="i"
+                v-on:remove-todo="removeTodo"
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem'
+export default {
+    props: [
+        'todos'
+    ],
+    methods: {
+        removeTodo(id){
+            this.$emit('remove-todo', id)
+        }
+    },
+    components: {
+        TodoItem
+    }
+}
+</script>
+
+<style>
+    ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+</style>
